319cec5eaba0f1af567ba40a39dd5a9edf52e54b
oncomouse <oncomouse@gmail.com>
2017-12-18 10:32:57 -0600

    Formatted ESLint Config File

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,46 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-	"globals": {
-		"process": false
-		, "APP_TITLE": false
-	}
-    , "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "rules": {
-		// I prefer commas at the start of lists; disable to turn off:
-		"comma-style": [
-			"error"
-			, "first"
-		]
-        , "indent": [
-            "error"
-            , 2
-        ]
-		// Enforce a max line-length of 80 chars:
-		, "max-len": [
-			"error"
-			, { "code": 80 }
-		]
-        , "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
+  'env': {
+    'browser': true
+    ,'commonjs': true
+    ,'es6': true
+  }
+  ,'extends': 'eslint:recommended'
+  ,'globals': {
+    'process': false
+    , 'APP_TITLE': false
+  }
+  , 'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
     }
-};
+    ,'sourceType': 'module'
+  }
+  ,'rules': {
+    // I prefer commas at the start of lists; disable to turn off:
+    'comma-style': [
+      'error'
+      , 'first'
+    ]
+    , 'indent': [
+      'error'
+      , 2
+    ]
+    // Enforce a max line-length of 80 chars:
+    , 'max-len': [
+      'error'
+      , { 'code': 80 }
+    ]
+    , 'linebreak-style': [
+      'error'
+      ,'unix'
+    ]
+    ,'quotes': [
+      'error'
+      ,'single'
+    ]
+    ,'semi': [
+      'error'
+      ,'never'
+    ]
+  }
+}
