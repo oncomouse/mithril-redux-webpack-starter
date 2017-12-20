356a18d2389012fe71c6ff057967856c4d1e83c7
oncomouse <oncomouse@gmail.com>
2017-12-20 09:40:15 -0600

    modularized addProps

--- app/utilities/mithril-props/addProps.js
+++ app/utilities/mithril-props/addProps.js
@@ -1,27 +1,25 @@
+const makeProps = function(vnode) {
+    return Object.assign(
+        {}
+        , vnode.state.defaultProps || {}
+        , vnode.attrs
+    )
+}
+const newOninit = function(vnode, cb) {
+    vnode.state.props = makeProps(vnode);
+    cb(vnode)
+}
+
 const addProps = function(Component) {
     if(typeof Component.oninit === 'function') {
         Component._prepropsOninit = Component.oninit
-        Component.oninit = function(vnode) {
-            vnode.state.props = Object.assign(
-                {}
-                , vnode.state.defaultProps || {}
-                , vnode.attrs
-            )
-            Component._prepropsOninit(vnode)
-        }
+        Component.oninit = newOninit(vnode,Component._prepropsOninit)
     } else if(
         Component.prototype
         && typeof Component.prototype.oninit === 'function'
     ) {
         Component.prototype._prepropsOninit = Component.prototype.oninit
-        Component.prototype.oninit = function(vnode) {
-            vnode.state.props = Object.assign(
-                {}
-                , vnode.state.defaultProps || {}
-                , vnode.attrs
-            )
-            this._prepropsOninit(vnode)
-        }
+        Component.prototype.oninit = newOninit(vnode, this._prepropsOninit)
     }
     return Component
 }
